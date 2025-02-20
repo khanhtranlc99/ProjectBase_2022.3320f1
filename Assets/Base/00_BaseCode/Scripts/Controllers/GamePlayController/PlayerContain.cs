c837a4853b462d5e76321ef9f1b651478f15c935
DESKTOP-USG9QUT\Admin <trankhanhlc99@gmail.com>
2025-02-20 18:26:21 +0700

    70%

--- Assets/Base/00_BaseCode/Scripts/Controllers/GamePlayController/PlayerContain.cs
+++ Assets/Base/00_BaseCode/Scripts/Controllers/GamePlayController/PlayerContain.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 public class PlayerContain : MonoBehaviour
 {
-    
+ 
+    public LevelData levelData; 
     public void Init()
     { 
+      levelData.Init();
+
     }
 
-   
-
-
+  
 }
